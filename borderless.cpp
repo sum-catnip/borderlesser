03c07f48cc9fd9ac453e75697d63ed4862b3bfc7
Freestyle-Coder <FreestyleC0der@protonmail.com>
2017-03-23 11:37:24 +0100

    fixed some bugs + window is not topmost anymore

--- borderless.cpp
+++ borderless.cpp
@@ -2,7 +2,7 @@
 #include <cstdio>
 
 HWND getWindow() {
-	printf("Press CRLT + H to make the currently focussed window borderless\n");
+	printf("Press CRLT + H to make the currently focussed window borderless");
 	MSG msg;
 	while (GetMessage(&msg, NULL, 0, 0) != 0) {
 		if (msg.message == WM_HOTKEY) {
@@ -19,10 +19,15 @@ int main() {
 	char windowTitle[100];
 	while (true) {
 		window = getWindow();
-		SetWindowLong(window, GWL_STYLE, 0x00000000);
-		SetWindowLong(window, GWL_STYLE, 0x94000000);
-		SetWindowPos(window, HWND_TOPMOST, 0, 0, screenWidth, screenHeigth, NULL);
-		GetWindowText(window, windowTitle, 100);
-		printf("Set window: %s to borderless\n\n", windowTitle);
+		if (window) {
+			GetWindowText(window, windowTitle, 100);
+			if (windowTitle) {
+				SetWindowLongPtr(window, GWL_EXSTYLE, 0x00000000);
+				SetWindowLongPtr(window, GWL_STYLE, 0x94000000);
+				SetWindowPos(window, HWND_TOP, 0, 0, screenWidth, screenHeigth, NULL);
+
+				printf("\nSet window: %s to borderless\n\n", windowTitle);
+			}
+		}
 	}
 }
